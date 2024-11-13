06bfc011f50230cbe8a50aa8ff597d0b0e6d180a
Pornchanok Pinnuwat <pearp.pinnuwat@gmail.com>
2024-11-13 07:52:26 +0000

    Add Unit test cases

--- test_zoo.py
+++ test_zoo.py
@@ -5,10 +5,29 @@ class TestZoo(unittest.TestCase):
     def setUp(self):
         self.zoo = Zoo()
 
-    def test_child_ticket_price(self):
+    def test_invalid_age(self): #F
+        self.assertEqual(self.zoo.get_ticket_price(-1), 'Invalid Age')
+
+    def test_age_between_0_and_12(self):
+        self.assertEqual(self.zoo.get_ticket_price(0), 50)
         self.assertEqual(self.zoo.get_ticket_price(5), 50)
-       
-    # Add your additional test cases here.
+        self.assertEqual(self.zoo.get_ticket_price(12), 50)
+
+    def test_age_between_13_and_20(self):
+        self.assertEqual(self.zoo.get_ticket_price(13), 100)
+        self.assertEqual(self.zoo.get_ticket_price(15), 100)
+        self.assertEqual(self.zoo.get_ticket_price(20), 100)
+
+    def test_age_between_21_and_60(self):
+        self.assertEqual(self.zoo.get_ticket_price(21), 150)
+        self.assertEqual(self.zoo.get_ticket_price(46), 150)
+        self.assertEqual(self.zoo.get_ticket_price(60), 150)
+
+    def test_age_greater_than_60(self):
+        self.assertEqual(self.zoo.get_ticket_price(61), 100)
+        self.assertEqual(self.zoo.get_ticket_price(69), 100)
+        self.assertEqual(self.zoo.get_ticket_price(74), 100)
+
 
 if __name__ == '__main__':
     unittest.main()
